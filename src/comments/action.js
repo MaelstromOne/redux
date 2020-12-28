e9c7c74b4fb0733b76419af645ca166611d97080
Albert Gabbasov <albertxgm@gmail.com>
2020-12-29 00:53:42 +0300

    Реализованы action

--- src/comments/action.js
+++ src/comments/action.js
@@ -1,7 +1,13 @@
-export const addComment = {
-    type: "ADD_COMMENT"
+export const addComment = (comment) => {
+    return {
+        type: "ADD_COMMENT",
+        payload: comment
+    }
 }
 
-export const deleteComment = {
-    type: "DELETE_COMMENT"
+export const deleteComment = (index) => {
+    return {
+        type: "DELETE_COMMENT",
+        payload: index
+    }
 }
