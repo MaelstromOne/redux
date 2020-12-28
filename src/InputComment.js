c4ea8ce9377e0770446e64c8765289bbdd16f404
Albert Gabbasov <albertxgm@gmail.com>
2020-12-29 01:06:47 +0300

    Рефакторинг

--- src/InputComment.js
+++ src/InputComment.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
-export default (props) => {
-    return (
-        <form className={"input"} onSubmit={props.add}>
-            <fieldset>
-                <legend>Добавить комментарий</legend>
-                <label>Имя</label>
-                <input type="text" name="name" value={props.name} onChange={props.chAuthor}/>
-                <label>Комментарий</label>
-                <textarea rows="10" cols="45" name="text" value={props.text} onChange={props.chText}/>
-                <button className={"send"}>Отправить</button>
-            </fieldset>
-        </form>
-    )
+export default class Comments extends React.Component {
+    render() {
+        return (
+            <form className={"input"} onSubmit={this.props.add}>
+                <fieldset>
+                    <legend>Добавить комментарий</legend>
+                    <label>Имя</label>
+                    <input type="text" name="name" value={this.props.name} onChange={this.props.chAuthor}/>
+                    <label>Комментарий</label>
+                    <textarea rows="10" cols="45" name="text" value={this.props.text} onChange={this.props.chText}/>
+                    <button className={"send"}>Отправить</button>
+                </fieldset>
+            </form>
+        )
+    }
 }
